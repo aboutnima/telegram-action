710fea3be788e3137faa7d137d5da80e129f9b07
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-07-30 16:15:48 +0330

    upload package

--- src/Services/TelegramActionService.php
+++ src/Services/TelegramActionService.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace Aboutnima\TelegramAction\Services;
 
-use Aboutnima\TelegramAction\Auth\ZoomTokenManager;
-use Aboutnima\TelegramAction\Contracts\Services\ZoomServiceInterface;
-use Aboutnima\TelegramAction\Exceptions\ZoomException;
-use Closure;
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Http\Client\Response;
-use Illuminate\Support\Facades\Http;
-
 final readonly class TelegramActionService
 {
     public function __construct() {}
+
+    public function default(): self
+    {
+        return $this;
+    }
 }
